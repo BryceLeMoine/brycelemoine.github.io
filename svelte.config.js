bfc74c7fc203dbdf9800d5abfe1c78dc8da04d4a
Bryce LeMoine <lemoine.bryce@heb.com>
2025-07-30 16:18:33 -0500

    fix: config changes

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,7 @@ const config = {
       strict: true,
     }),
     paths: {
-      base: process.argv.includes("dev") ? "" : "/seo-test",
+      base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
     },
   },
 };
